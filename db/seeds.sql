3dba156639411699fdc763127b3d548414c3b3ec
shinyscyther <mesibovj@gmail.com>
2019-02-11 18:02:17 -0500

    redeleting old files

--- db/seeds.sql
+++
@@ -1,9 +0,0 @@
-USE todos_db;
-
-INSERT INTO todos
-  (todo)
-VALUES
-  ("Create more todos"),
-  ("Finish app"),
-  ("Win over my class");
-  
--- routes/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const todoRoutes = require("./todoRoutes");
-
-// set up routes and prefix them
-router.use("/todos", todoRoutes);
-
-
-module.exports = router;
